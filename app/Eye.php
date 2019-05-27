779204dab54f37e50a2f1d3f0a1648720e842583
Carlos Campos <carlos2954@hotmail.com>
2019-05-26 21:13:42 -0300

    creation of models and migrations

---
+++ app/Eye.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Eye extends Model
+{
+    public $timestamps = false;
+}
---
+++ app/Mouth.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mouth extends Model
+{
+    public $timestamps = false;
+}
