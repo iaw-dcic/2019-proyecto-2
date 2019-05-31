5ffcd1ac87468c91c76542fd9d2ef13807b5aace
JoseOchoaUns <joseochoa.uns@gmail.com>
2019-05-31 00:50:53 -0300

    Primer parte del routing hecha

--- public/js/app.js
+++ public/js/app.js
@@ -65683,17 +65683,19 @@ if (token) {
 /*!****************************************!*\
   !*** ./resources/js/components/App.js ***!
   \****************************************/
-/*! no exports provided */
+/*! exports provided: history */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "history", function() { return history; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-/* harmony import */ var _Prode__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Prode */ "./resources/js/components/Prode.js");
+/* harmony import */ var history__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! history */ "./node_modules/history/esm/history.js");
+/* harmony import */ var _Prode__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./Prode */ "./resources/js/components/Prode.js");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -65704,18 +65706,22 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
 
+
+
+var history = Object(history__WEBPACK_IMPORTED_MODULE_3__["createHashHistory"])();
 
 var App =
 /*#__PURE__*/
@@ -65723,15 +65729,67 @@ function (_Component) {
   _inherits(App, _Component);
 
   function App() {
+    var _this;
+
     _classCallCheck(this, App);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this));
+
+    _defineProperty(_assertThisInitialized(_this), "nuevoProde", function () {
+      console.log("entro");
+
+      _this.context.history.push('/prode');
+    });
+
+    _this.state = {
+      partidosDelUsuario: []
+    };
+    return _this;
   }
 
   _createClass(App, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      axios.get("/partidos").then(function (response) {
+        console.log(response["data"]);
+        var partidosDelUsuario = response["data"];
+
+        _this2.setState({
+          partidosDelUsuario: partidosDelUsuario
+        });
+      })["catch"](function (error) {
+        console.log("this is error", error);
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["BrowserRouter"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Prode__WEBPACK_IMPORTED_MODULE_3__["default"], null)));
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["BrowserRouter"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "container"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "row"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "col-md-offset-3"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "btn-group col-md-offset-3"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Link"], {
+        to: "/prode"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        type: "button",
+        className: "btn btn-primary text-center center"
+      }, "Nuevo prode")), this.state.partidosDelUsuario.map(function (lista) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+          type: "button",
+          className: "btn btn-primary text-center center",
+          key: lista.id,
+          value: lista.id
+        }, "Prode existente");
+      }))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
+        path: "/prode",
+        component: _Prode__WEBPACK_IMPORTED_MODULE_4__["default"]
+      }));
     }
   }]);
 
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switchm, Link } from 'react-router-dom'
+import { createHashHistory } from 'history'
 import Prode from './Prode'
-
+export const history = createHashHistory();
 class App extends Component {
-    render () {
-    return (
-        <BrowserRouter>
-        <div>
-            <Prode />
-        </div>
-        </BrowserRouter>
-    )
+
+
+
+    constructor() {
+        super();
+        this.state = {
+            partidosDelUsuario: [],
+        }
+    }
+
+    componentDidMount() {
+
+        axios.get("/partidos").then(response => {
+            console.log(response["data"]);
+            var partidosDelUsuario = response["data"];
+            this.setState({ partidosDelUsuario });
+        }).catch(error => {
+            console.log("this is error", error);
+        });
+    }
+
+    nuevoProde = () => {
+        console.log("entro");
+        this.context.history.push('/prode')
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-offset-3">
+                            <div className="btn-group col-md-offset-3">
+                                <Link to="/prode">
+                                    <button type="button" className="btn btn-primary text-center center">Nuevo prode</button>
+                                </Link>
+                                {
+                                    this.state.partidosDelUsuario.map(lista => {
+                                        return <button type="button" className="btn btn-primary text-center center" key={lista.id} value={lista.id}>Prode existente</button>
+                                    })
+                                }
+                                {/* <button type="button" className="btn btn-primary text-center center">Samsung</button>
+                                <button type="button" className="btn btn-primary text-center center">Sony</button> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Route path="/prode" component={Prode}></Route>
+
+            </BrowserRouter>
+        )
     }
 }
 
