5d73c6a78cd90b20e875dba96ddbae5fb2bab4e7
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-06-02 23:56:53 -0300

    primera parte del local storage

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products','ProductsController@index');
-
-Route::post('products','ProductsController@store')->middleware('auth');
-
-Route::get('products/{id}','ProductsController@show');
-
-Route::put('products/{id}','ProductsController@update');
-
-Route::delete('products/{id}','ProductsController@delete');
-
-Route::get('colors', 'ColorsController@index');
-
-Route::get('images', 'ImagesController@index');
-
-Route::get('fundas', 'FundasController@index');
-
-Route::get('colorcase/{id_case}/{id_color}', 'ColorCasesController@show');
