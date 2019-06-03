4ebcfb759d0a5cb2103b61e224c3bbb2e99b9842
joaco1916 <joaquin_a_rodriguez@hotmail.com>
2019-06-03 15:55:56 -0300

    Routes
    Agregadas todas las routes necesarias para el manejo de prodes.

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,13 @@
 
 
 Auth::routes();
+Route::view('/', 'react');
 Route::view('/{path?}', 'react')->middleware('auth');
+
+Route::resource('users', 'UserController')->middleware('auth');
+
+Route::get('api/prode','Api\ProdeController@index')->middleware('auth');
+Route::post('api/prode/store','Api\ProdeController@store')->middleware('auth');
+Route::delete('api/prode/delete/{id}','Api\ProdeController@destroy')->middleware('auth');
+Route::get('api/prode/edit/{id}','Api\ProdeController@edit')->middleware('auth');
+Route::put('api/prode/update/{id}','Api\ProdeController@update')->middleware('auth');
