d7ee7fcafab926814ffa78c524298af8be7fef62
Jonathan Fritz <jonathanfritz92@gmail.com>
2019-06-04 16:56:43 -0300

    fix XMLHttpRequest to origin

--- public/js/app.js
+++ public/js/app.js
@@ -65650,7 +65650,7 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://iaw-dcic-proyecto2-fritz.herokuapp.com';
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
