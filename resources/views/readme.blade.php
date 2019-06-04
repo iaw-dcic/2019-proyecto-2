21b742a4627f8f80ce7e7b01edb68b046622724f
JuanmaFloyd <juanma_giron@hotmail.es>
2019-06-04 16:02:29 -0300

    Update readme.blade.php

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 @section('content')
+	<h1>Readme</h1>
     <div class="containter">
     	<ul>
     		<li>El layout general fue realizado con Bootstrap</li>
     		<li>Se utilizó la librería Axios para facilitar la comunicación HTTP</li>
+    		<li>El botón "guardar cambios" crea una nueva entrada en la BD si se trabaja en un prode nuevo o modifica un prode si se trabaja sobre un prode seleccionado ya guardado</li>
     	</ul>
+    </div>
+    <div class="container">
+    	<p>Proyecto de Juan Martín Girón - 114157</p>
     </div>
 @endsection
