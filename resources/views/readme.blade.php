b414b191b2ebc2bccf1f1f81afd76fa1d0611593
JuanmaFloyd <juanma_giron@hotmail.es>
2019-06-04 15:56:55 -0300

    Update readme.blade.php

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <h1>asd</h1>
+    <div class="containter">
+    	<ul>
+    		<li>El layout general fue realizado con Bootstrap</li>
+    		<li>Se utilizó la librería Axios para facilitar la comunicación HTTP</li>
+    	</ul>
+    </div>
 @endsection
