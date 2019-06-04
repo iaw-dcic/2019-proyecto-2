a2b7a609d0de90a3f7d0e138f51fef0602e10aab
mmassetti <matiasmassetti@gmail.com>
2019-06-04 17:22:37 -0300

    Added /readme

---
+++ resources/views/readme.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!--404 Not Found -->
+    <link rel="stylesheet" href="{{ URL::asset('/css/readme.css') }}">
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div id="notfound">
+    <div class="notfound">
+
+        <h2>Autor: Matias Massetti - LU: 107954</h2>
+        <a href="/">Volver al inicio</a>
+    </div>
+</div>
+
+</body>
+
+</html>
