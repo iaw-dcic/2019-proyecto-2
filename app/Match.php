a71b60b073e411f3a1178333fb09ef0d3b98b75d
Micaelamelo <melomicaelaa@gmail.com>
2019-06-05 00:10:17 -0300

    hehe

--- app/Match.php
+++ app/Match.php
@@ -3,10 +3,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Match extends Model
 {
-    protected $fillable = [
-        'ronda',
-        'team1_id'
-        'team2_id'
-        'prediction'
+    protected $fillable = [ //lo agrego despues cuando resuelva lo de guardar partidos porque no me muestra lo de la bd sino
+      //  'ronda',
+      //  'team1_id'
+      //  'team2_id'
+      //  'prediction'
     ];
 }
