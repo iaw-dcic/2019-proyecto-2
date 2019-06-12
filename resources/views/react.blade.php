9ff0fddbcd7e1ead85f3ad5ea78c4a35c652e4ea
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-06-12 20:43:35 -0300

    Se arreglaron los labels de blade

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ _('FundaMaker') }}</title>
+    <title>FundaMaker</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
