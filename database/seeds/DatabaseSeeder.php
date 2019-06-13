bfb8ac509fc4a260ddb40e71faa462e800e11a84
Constanza Giorgetti <coti.giorgetti@hotmail.com>
2019-06-12 22:22:04 -0300

    se arreglaron los id de los seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,122 +18,122 @@ class DatabaseSeeder extends Seeder
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '2',
+        'id_color' => '11',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-amarillo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '3',
+        'id_color' => '21',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-verde.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '4',
+        'id_color' => '31',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-teal.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '5',
+        'id_color' => '41',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-blue.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '6',
+        'id_color' => '51',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-indigo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '7',
+        'id_color' => '61',
         'id_case' => '1',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhone7-8/iPhone7-8-blanco.png',
       ]);
 
       DB::table('cases_color')->insert([
         'id_color' => '1',
-        'id_case' => '2',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-coral.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '2',
-        'id_case' => '2',
-        'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-coral.png',
+        'id_color' => '11',
+        'id_case' => '11',
+        'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-amarillo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '3',
-        'id_case' => '2',
+        'id_color' => '21',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-verde.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '4',
-        'id_case' => '2',
+        'id_color' => '31',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-teal.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '5',
-        'id_case' => '2',
+        'id_color' => '41',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-blue.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '6',
-        'id_case' => '2',
+        'id_color' => '51',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-indigo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '7',
-        'id_case' => '2',
+        'id_color' => '61',
+        'id_case' => '11',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneX/iPhoneX-blanco.png',
       ]);
 
       DB::table('cases_color')->insert([
         'id_color' => '1',
-        'id_case' => '3',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-coral.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '2',
-        'id_case' => '3',
+        'id_color' => '11',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-amarillo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '3',
-        'id_case' => '3',
+        'id_color' => '21',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-verde.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '4',
-        'id_case' => '3',
+        'id_color' => '31',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-teal.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '5',
-        'id_case' => '3',
+        'id_color' => '41',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-blue.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '6',
-        'id_case' => '3',
+        'id_color' => '51',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-indigo.png',
       ]);
 
       DB::table('cases_color')->insert([
-        'id_color' => '7',
-        'id_case' => '3',
+        'id_color' => '61',
+        'id_case' => '21',
         'path' => 'https://iawbucket.s3.amazonaws.com/coti/imagenes-proyecto2/iPhoneXR/iPhoneXR-blanco.png',
       ]);
     }
