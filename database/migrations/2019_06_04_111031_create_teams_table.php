586cf3d7e9f25634226819ba217f5388d52a9fbd
RamiroFraysse <ramirofraysse@gmail.com>
2019-06-13 14:47:55 -0300

    heroku

--- database/migrations/2019_06_04_111031_create_teams_table.php
+++ database/migrations/2019_06_04_111031_create_teams_table.php
@@ -14,7 +14,7 @@ class CreateTeamsTable extends Migration
     public function up()
     {
         Schema::create('teams', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id');
             $table->string('name');
             $table->string('pais');
             $table->timestamps();
--- database/seeds/TeamSeeder.php
+++ database/seeds/TeamSeeder.php
@@ -17,80 +17,111 @@ class TeamSeeder extends Seeder
     public function run()
     {
         Team::create([
+            'id' => 1,
             'name' => 'River Plate',
             'pais' => 'Arg'
         ]);
         Team::create([
+            'id' => 2,
+
             'name' => 'Cruzeiro',
             'pais' => 'Bra'
 
         ]);
         Team::create([
+            'id' => 3,
+
             'name' => 'San Lorenzo',
             'pais' => 'Arg'
 
         ]);
         Team::create([
+            'id' => 4,
+
             'name' => 'Cerro Porteño',
             'pais' => 'Par'
 
         ]);
         Team::create([
+            'id' => 5,
+
             'name' => 'Liga Quito',
             'pais' => 'Ec'
 
         ]);
         Team::create([
+            'id' => 6,
+
             'name' => 'Olimpia',
             'pais' => 'Par'
 
         ]);
         Team::create([
+            'id' => 7,
+
             'name' => 'Paranaense',
             'pais' => 'Bra'
 
         ]);
         Team::create([
+            'id' => 8,
+
             'name' => 'Boca Juniors',
             'pais' => 'Arg'
 
         ]);
         Team::create([
+            'id' => 9,
+
             'name' => 'Godoy Cruz',
             'pais' => 'Arg'
 
         ]);
         Team::create([
+            'id' => 10,
+
             'name' => 'Palmeiras',
             'pais' => 'Bra'
 
         ]);
         Team::create([
+            'id' => 11,
+
             'name' => 'Gremio',
             'pais' => 'Bra'
 
         ]);
         Team::create([
+            'id' => 12,
+
             'name' => 'Libertad',
             'pais' => 'Par'
 
         ]);
         Team::create([
+            'id' => 13,
+
             'name' => 'Emelec',
             'pais' => 'Ec'
 
         ]);
         Team::create([
+            'id' => 14,
+
             'name' => 'Flamengo',
             'pais' => 'Bra'
 
         ]);
         Team::create([
+            'id' => 15,
+
             'name' => 'Nacional',
             'pais' => 'Ur'
 
         ]);
         Team::create([
+            'id' => 16,
+
             'name' => 'Internacional',
             'pais' => 'Bra'
 
