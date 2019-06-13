1139a2e042959af7f6a48490e4f35d0c8a29d089
Danilo Pesce <danilo9.p@hotmail.com>
2019-06-13 20:15:32 -0300

    Problema CSS heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    
+    <!-- Styles --> 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
