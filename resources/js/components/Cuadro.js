61669248c5832dca8f1be568b9d48e7ab23a2b37
Micaelamelo <melomicaelaa@gmail.com>
2019-10-22 14:11:17 -0300

    mejor

--- resources/js/components/Cuadro.js
+++ resources/js/components/Cuadro.js
@@ -318,9 +318,7 @@ componentWillMount() { //LOCAL STORAGE
       this.setState({
         teams: response.data
       });
-      var found = teams.find(function(element) {
-          return element.id==i;
-      });
+      let found = teams.find(element => element.id == i);
 
       return found.name;
     })
@@ -330,7 +328,7 @@ componentWillMount() { //LOCAL STORAGE
       return this.state.matches.map(match => {
        return(
               <li key={match.num}  className="team-item">
-                    <button id={match.team1_id} type="button" className= "btn btn-info" onClick={(e) => this.onClick1(e,match.team1_id, match.num-1)}>{this.getNombre(match.team1_id)}</button>
+                    <button id={match.team1_id} type="button" className= "btn btn-info" onClick={(e) => this.onClick1(e,match.team1_id, match.num-1)}>{match.team1_id}</button>
                       <time>vs</time>
                     <button  id={match.team2_id} type="button" className= "btn btn-info" onClick={(e) => this.onClick1(e,match.team2_id,match.num-1)} type="button">{match.team2_id}</button>
               </li>
